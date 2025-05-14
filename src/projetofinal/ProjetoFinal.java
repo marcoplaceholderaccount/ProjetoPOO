f0cd1f5ea4a401725793066c9c3c1a931509de44
marco <marco@Marco-PC>
2025-05-14 17:11:55 -0100

    Primeiro Commit

---
+++ src/projetofinal/ProjetoFinal.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projetofinal;
+import frontend.telaLogin;
+
+/**
+ *
+ * @author marco
+ */
+public class ProjetoFinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        telaLogin adm = new telaLogin();
+        adm.setVisible(true);
+        
+    }
+    
+}
